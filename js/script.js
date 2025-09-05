341dd5b2b07a13de45698e79e55fc1a4aaa3a14a
Harsh Singh <harshsingh1822006@gmail.com>
2025-09-05 14:03:40 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+window.onload = function () {
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+  }
+
+  let darkModeButton = document.createElement("button");
+  darkModeButton.id = "darkModeButton";
+  darkModeButton.innerText = "Toggle Dark Mode";
+  darkModeButton.onclick = toggleDarkMode;
+  document.body.appendChild(darkModeButton);
+};
